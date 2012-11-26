c7c9456204ef6d6015b38f8e26fb93201df87591
Richard Toolan <rtoolan91@gmail.com>
2012-11-26 12:59:23 +0000

    Initial Commit

---
+++ Campus Navigator/CNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CNAppDelegate.h
+//  Campus Navigator
+//
+//  Created by Rich on 26/11/2012.
+//  Copyright (c) 2012 UCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
